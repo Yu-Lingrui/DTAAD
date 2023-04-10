c65b0d354c1b15bb296aa3606969411c3c95a902
余翎瑞 <15282590579@163.com>
2023-04-10 20:20:55 +0800

    Update models.py

--- src/models.py
+++ src/models.py
@@ -364,7 +364,44 @@ class MAD_GAN(nn.Module):
         fake_score = self.discriminator(z.view(1, -1))
         return z.view(-1), real_score.view(-1), fake_score.view(-1)
 
+    
+# Proposed Model + Self Conditioning + Adversarial + MAML (VLDB 22)
+class TranAD(nn.Module):
+    def __init__(self, feats):
+        super(TranAD, self).__init__()
+        self.name = 'TranAD'
+        self.lr = lr
+        self.batch = 128
+        self.n_feats = feats
+        self.n_window = 10
+        self.n = self.n_feats * self.n_window 
+        self.pos_encoder = PositionalEncoding(2 * feats, 0.1, self.n_window) 
+        encoder_layers = TransformerEncoderLayer(d_model=2 * feats, nhead=feats, dim_feedforward=16, dropout=0.1)
+        self.transformer_encoder = TransformerEncoder(encoder_layers, 1)  
+        decoder_layers1 = TransformerDecoderLayer(d_model=2 * feats, nhead=feats, dim_feedforward=16, dropout=0.1)
+        self.transformer_decoder1 = TransformerDecoder(decoder_layers1, 1)
+        decoder_layers2 = TransformerDecoderLayer(d_model=2 * feats, nhead=feats, dim_feedforward=16, dropout=0.1)
+        self.transformer_decoder2 = TransformerDecoder(decoder_layers2, 1)
+        self.fcn = nn.Sequential(nn.Linear(2 * feats, feats), nn.Sigmoid())
 
+    def encode(self, src, c, tgt):
+        src = torch.cat((src, c), dim=2)
+        src = src * math.sqrt(self.n_feats)
+        src = self.pos_encoder(src)
+        memory = self.transformer_encoder(src)
+        tgt = tgt.repeat(1, 1, 2)
+        return tgt, memory
+
+    def forward(self, src, tgt):
+        # Phase 1 - Without anomaly scores
+        c = torch.zeros_like(src)
+        x1 = self.fcn(self.transformer_decoder1(*self.encode(src, c, tgt)))
+        # Phase 2 - With anomaly scores
+        c = (x1 - src) ** 2  # 广播机制
+        x2 = self.fcn(self.transformer_decoder2(*self.encode(src, c, tgt)))
+        return x1, x2  
+
+    
 # Proposed Model + Tcn_Global + Transformer + MAML
 class DTAAD_Tcn_Local(nn.Module):
     def __init__(self, feats):
